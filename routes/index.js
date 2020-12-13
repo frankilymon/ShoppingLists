e21555c6e83deb4c43ab4e13fff062fdfd4a89e4
Vikram Kumar <vikram109kumar@gmail.com>
2020-12-12 22:39:11 -0500

    Heading and Port
    Changed the port to 5000 and Set the heading of the page to Shopping List

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const db = require('better-sqlite3')
+const app = express();
+//const db = require('better-sqlite3')
 
-/* GET home page. */
+const PORT = process.env.PORT || 5000;
+
+/* GET home page. 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: 'Shopping List' });
+}); */
+app.get('/', (req, res) => {
+  res.send('<h1>Shopping List</h1>')
+})
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`))
 
 module.exports = router;
