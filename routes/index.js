9e788ce6f52d570c8e4e5ca86d6dadf02163ddd5
Vikram Kumar <vikram109kumar@gmail.com>
2020-12-13 03:28:54 -0500

    App.js and cart.js edited

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,52 @@
 var express = require('express');
 var router = express.Router();
-const app = express();
-//const db = require('better-sqlite3')
 
-const PORT = process.env.PORT || 5000;
+var fs = require('fs');
 
-// GET home page. 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shopping List' });
-}); 
-/*app.get('/', (req, res) => {
-  res.send('<h1>Shopping List</h1>')
-})*/
+var Cart = require('../models/cart');
+var products = JSON.parse(fs.readFileSync('./data/products.json', 'utf8'));
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`))
+router.get('/', function (req, res, next) {
+  res.render('index', 
+  { 
+    title: 'Shopping Cart',
+    products: products
+  }
+  );
+});
+
+router.get('/add/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+  var product = products.filter(function(item) {
+    return item.id == productId;
+  });
+  cart.add(product[0], productId);
+  req.session.cart = cart;
+  res.redirect('/');
+});
+
+router.get('/cart', function(req, res, next) {
+  if (!req.session.cart) {
+    return res.render('cart', {
+      products: null
+    });
+  }
+  var cart = new Cart(req.session.cart);
+  res.render('cart', {
+    title: 'Shopping Cart',
+    products: cart.getItems(),
+    totalPrice: cart.totalPrice
+  });
+});
+
+router.get('/remove/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  cart.remove(productId);
+  req.session.cart = cart;
+  res.redirect('/cart');
+});
 
 module.exports = router;
